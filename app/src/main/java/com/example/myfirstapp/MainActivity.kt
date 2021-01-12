72010bb1d9ee7106eb3e35a122f8e83151e7e74a
Rivendor <flimarques26@gmail.com>
2021-01-11 22:19:14 -0300

    New feature: Material Design Text Field that changes the TextView with a Int. If the field is empty, it changes to '0'.

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.decButton).setOnClickListener {
             decrementValue()
         }
+        findViewById<Button>(R.id.inputButton).setOnClickListener {
+            overwriteValue()
+        }
     }
 
     private fun incrementValue() {
@@ -30,6 +35,13 @@ class MainActivity : AppCompatActivity() {
     private fun decrementValue() {
         findViewById<TextView>(R.id.changeableValue).apply {
             text = (text.toString().toInt() - 1).toString()
+        }
+    }
+
+    private fun overwriteValue() {
+        val newValue = findViewById<TextInputLayout>(R.id.textField).editText?.text
+        findViewById<TextView>(R.id.changeableValue).apply {
+            text = if(newValue.isNullOrEmpty()) "0" else newValue
         }
     }
 }
